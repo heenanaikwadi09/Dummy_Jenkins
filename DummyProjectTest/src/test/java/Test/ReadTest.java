ef9e6491f737a77d69f72ad8b7c6b92836509078
Heena <Heena@LAPTOP-PFDOKATC>
2025-04-06 18:28:35 +0530

    Dummy Project Commit

---
+++ DummyProjectTest/src/test/java/Test/ReadTest.java
@@ -0,0 +1,13 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class ReadTest {
+	
+	@Test
+    public void ReadTest()
+    {
+    	System.out.println("Read Test executed successfully");
+    }
+
+}
---
+++ DummyProjectTest/src/test/java/Test/WriteTest.java
@@ -0,0 +1,13 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class WriteTest {
+	
+	@Test
+    public void WriteTest()
+    {
+    	System.out.println("Write Test executed successfully");
+    }
+
+}
